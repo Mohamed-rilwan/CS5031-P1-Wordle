cabf49ba67304e55a4d0320d2e489283388fcf70
MOHAMED RILWAN S <mohamed.rilwan14@gmail.com>
2023-01-30 14:12:22 +0000

    The following implementations were added: 1. Test 1 - Fetch word from the given link 2. Test 2 - Pick random word from the given file path 3. Implementation of the above functions

--- wordle/src/main/java/stacs/wordle/WordleApp.java
+++ wordle/src/main/java/stacs/wordle/WordleApp.java
@@ -1,8 +1,12 @@
 package stacs.wordle;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 import java.nio.file.Path;
 
@@ -13,10 +17,43 @@ public class WordleApp
         System.out.println("Welcome to CS5031 - Wordle");
     }
 
-    // Unimplemented skeleton
-    // You may refactor this method
-    protected static ArrayList<String> loadWordlist(String wordlistPath) throws FileNotFoundException
+    /**
+     * This method is used read a file using a file path
+     * and return a list of words in it as an array
+     *
+     * @return list of all words in the given file.
+     * @throws FileNotFoundException when the file is not found in the given
+     *                               location.
+     */
+     protected static ArrayList<String> loadWordlist(String wordlistPath) throws FileNotFoundException
     {
-        return new ArrayList<String>();
+        ArrayList<String> words = new ArrayList<>();
+        File textFile = new File(wordlistPath);
+        try {
+            Scanner fileReader = new Scanner(textFile);
+            while(fileReader.hasNextLine()) {
+                words.add(fileReader.nextLine());
+            }
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            Path path = Paths.get(wordlistPath);
+            Path fileName = path.getFileName();
+            throw new FileNotFoundException("File not found: " + fileName.toString());
+        }
+        return words;
+    }
+
+    /**
+     * This method is used read a file using a given file path and return a word at random
+     * from that given file path.
+     * @param filePath - indicates the file path for all possible five-letter words
+     * @return random selected  word from list of pre-defined words.
+     * @throws FileNotFoundException when the file is not found in the given
+     *                               location.
+     */
+    public static String randomWordSelector(String filePath) throws FileNotFoundException {
+        String wordForTheDay = "";
+        ArrayList<String> words = loadWordlist(filePath);
+        return words.get(new Random().nextInt(words.size()));
     }
 }
--- wordle/src/test/java/stacs/wordle/WordleAppTest.java
+++ wordle/src/test/java/stacs/wordle/WordleAppTest.java
@@ -13,9 +13,17 @@ public class WordleAppTest
     @Test
     public void shouldLoadWordlist() throws FileNotFoundException
     {
-        ArrayList<String> wordlist = WordleApp.loadWordlist("src/test/resources/wordlist-test.txt");
+        ArrayList<String> wordlist = WordleApp.loadWordlist("D:\\University_Of_St_Andrews\\Semester 2\\CS5031 - Software Engineering Practice\\Coursework\\P1-Wordle\\CS5031-P1-Wordle\\wordle\\src\\test\\resources\\wordlist-test.txt");
 
         // test wordlist only contains 3 words, so wordlist should have the size of 3
         assertEquals(3, wordlist.size());
+    }
+
+    @Test
+    public void fetchRandomWord() throws FileNotFoundException
+    {
+        String randomWord = WordleApp.randomWordSelector("D:\\University_Of_St_Andrews\\Semester 2\\CS5031 - Software Engineering Practice\\Coursework\\P1-Wordle\\CS5031-P1-Wordle\\wordle\\src\\test\\resources\\wordlist-test.txt");
+        assertNotNull(randomWord);
+
     }
 }
